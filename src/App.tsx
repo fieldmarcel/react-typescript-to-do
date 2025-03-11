821bebe0b293da2de398dc2f0c1e42a4a4c38ab1
Shivanshu <143062839+fieldmarcel@users.noreply.github.com>
2025-03-11 18:58:44 +0000

    completed

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,18 @@ import { useState } from "react";
 export default function App() {
   const [todo, setTodo] = useState("");
   const [list, setList] = useState<string[]>([]);
-  const [checked, setchecked] = useState<string[]>([]);
-
+  const [checked, setchecked] = useState(false);
   const handlesubmit = () => {
     setList([...list, todo]);
     setTodo("");
+  };
+  const handlecheck = () => {};
+
+  const handledelete = (index: number) => {
+    // const newlist = list.filter((_, i) => i !== index);
+    // setList(newlist);
+    const newlist = list.filter((_, i) => i !== index);
+    setList(newlist);
   };
 
   return (
@@ -22,16 +29,19 @@ export default function App() {
             setTodo(e.target.value);
           }}
         ></input>
-        <button onClick={handlesubmit}>submit</button>
+        <button onClick={() => handlesubmit()}>submit</button>
       </div>
-      {list.map((task, index) => {
-        return (
-          <div className="flex flex-row">
-            <div key={index}> {task}</div>
-            <input type="checkbox"></input>
-          </div>
-        );
-      })}
+
+      {list.map((task, index) => (
+        <div key={index} className="flex  items-center gap-2">
+          <input type="checkbox" onClick={handlecheck} />
+
+          <span> {task}</span>
+          <button className=" gap-2 " onClick={() => handledelete(index)}>
+            del
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
